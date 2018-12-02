30972dff9ffe4c957416863abed64a764780e8e6
n3su <salted.mail@gmail.com>
2018-12-02 08:30:21 +0200

    Pridėta registracija.

--- Application/Controllers/API/ClientController.cs
+++ Application/Controllers/API/ClientController.cs
@@ -16,6 +16,46 @@ namespace Application.Controllers.API
     {
         public ClientController(BookstoreContext context, IMapper mapper) : base(context, mapper) {}
 
+        [AllowAnonymous]
+        [Route("register")]
+        [HttpPost]
+        public IActionResult Register(RegisterClientData request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            int mails = _context.Clients.Count(c => c.Email == request.Mail);
+            int usernames = _context.Clients.Count(c => c.Username == request.Username);
+
+            if (usernames > 0)
+            {
+                return BadRequest(new
+                {
+                    Username = new List<string> { "Vartotojas vardas užimtas." } 
+                });
+            }
+
+
+            if (mails > 0)
+            {
+                return BadRequest(new
+                {
+                    Mail = new List<string> { "Vartotojas šiuo el. paštu jau registruotas." }
+                });
+            }
+
+            _context.Clients.Add(new Client
+            {
+                Username = request.Username,
+                Password = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                Email = request.Mail,
+                AccessFlag = 0
+            });
+
+            _context.SaveChanges();
+            return Ok(new { message = "Nauja paskyra sėkmingai sukurta. Galite prisijungti." });
+        }
+
         [Authorize]
         [Route("modify/mail")]
         [HttpPost]
---
+++ Application/Models/Specifics/RegisterClientData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models.Specifics
+{
+    public class RegisterClientData
+    {
+        [Required(ErrorMessage = "Privalote įvesti vartotojo vardą.")]
+        [MinLength(6, ErrorMessage = "Vartotojo vardas turi susidaryti bent iš 6 simbolių.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Privalote įvesti slaptažodį.")]
+        [MinLength(6, ErrorMessage = "Slaptažodis turi susidaryti bent iš 6 simbolių.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Privalote įvesti el. pašto adresą.")]
+        [EmailAddress(ErrorMessage = "Neteisingas el. pašto adreso formatas.")]
+        public string Mail { get; set; }
+    }
+}
